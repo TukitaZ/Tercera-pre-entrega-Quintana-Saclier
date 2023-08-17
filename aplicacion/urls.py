51c0601de6b9be843a66295503db0208015468a2
ElianSaclier <eliansaclier@gmail.com>
2023-08-17 20:29:52 -0300

    "Version 0.2"

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('', home, name='home'),
+    path('materias/', materias, name='materias'),
+    path('maestros/', maestros, name='maestros'),
+    path('alumnos/', alumnos, name='alumnos'),
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Materia
 
 # Create your views here.
 def home(request):
     return render(request, 'aplicacion/home.html')
+
+def materias(request):
+    contexto = {'materias': Materia.objects.all()}
+    return render(request, 'aplicacion/materias.html', contexto)
+
+def maestros(request):
+    return render(request, 'aplicacion/maestros.html')
+
+def alumnos(request):
+    return render(request, 'aplicacion/alumnos.html')

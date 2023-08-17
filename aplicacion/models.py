2d0a8fd5259426e490da208a6d1ff5a21a18451b
ElianSaclier <eliansaclier@gmail.com>
2023-08-17 17:08:47 -0300

    Version 0.1

---
+++ aplicacion/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50)
+    comision = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre}'
+    
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    documento = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
+
+class Maestro(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    documento = models.IntegerField()
+    email = models.EmailField()
+    profesion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
